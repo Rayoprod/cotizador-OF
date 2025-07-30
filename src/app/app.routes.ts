f5d8ea6b538d7871abf732524963be7550a2a6a3
Agrovictoria <ti.agrovictoria@gmail.com>
2025-07-30 12:32:25 -0500

    a
    actualizacxion 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { QuoteCreator} from './components/quote-creator/quote-creator'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: QuoteCreator }
+];
---
+++ src/app/components/quote-creator/quote-creator.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-creator',
+  imports: [],
+  templateUrl: './quote-creator.html',
+  styleUrl: './quote-creator.scss'
+})
+export class QuoteCreator {
+
+}
