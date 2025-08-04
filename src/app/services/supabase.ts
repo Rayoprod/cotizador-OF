500d361478fc45346ce2f0d41f907a5ba3e504ef
Agrovictoria <ti.agrovictoria@gmail.com>
2025-08-04 16:30:20 -0500

    x
    x

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment'; // <-- 1. AQUÍ SE IMPORTA
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,7 @@ export class SupabaseService {
   public supabase: SupabaseClient;
 
   constructor() {
+    // 2. Y AQUÍ SE USA PARA CREAR LA CONEXIÓN
     this.supabase = createClient(
       environment.supabaseUrl,
       environment.supabaseKey
