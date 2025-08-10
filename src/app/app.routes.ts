ab8ff3f437d6d65ee5476e2cabdb150d07ee88f5
Agrovictoria <rrodriguez.1507w@gmail.com>
2025-08-10 06:12:37 -0500

    agregar componente adm
    agregar componente adm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { QuoteCreator } from './components/quote-creator/quote-creator';
 import { QuoteHistoryComponent } from './components/quote-history/quote-history'; // <-- IMPORTA EL NUEVO COMPONENTE
+import { AdminClientes } from './components/admin-clientes/admin-clientes';
 
 export const routes: Routes = [
     { path: '', component: QuoteCreator },
-    { path: 'historial', component: QuoteHistoryComponent } // <-- AÑADE LA NUEVA RUTA
+    { path: 'historial', component: QuoteHistoryComponent }, // <-- AÑADE LA NUEVA RUTA
+    { path: 'admin/clientes', component: AdminClientes, title: 'Administrar Clientes' }
+
 ];
