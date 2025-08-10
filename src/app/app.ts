7982ef85b8cb5c021d4fe919d08fe03313bb0f1c
Agrovictoria <rrodriguez.1507w@gmail.com>
2025-08-10 06:52:27 -0500

    a
    a

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router'; // <-- AÑADIR RouterModule
 import { ToastsComponent } from "./components/toasts/toasts";
 import { CommonModule } from '@angular/common';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap'; // <-- IMPORTA ESTO
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastsComponent,CommonModule,RouterModule],
+  imports: [RouterOutlet, ToastsComponent,CommonModule,RouterModule,NgbCollapseModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('cotizador-OF');
+    isMenuCollapsed = true; // <-- AÑADE ESTA LÍNEA
+
 }
