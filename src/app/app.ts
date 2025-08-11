147fb6858f078a08c72667e1a2fd9db46770670e
Agrovictoria <rrodriguez.1507w@gmail.com>
2025-08-11 15:53:46 -0500

    a
    aa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal,ChangeDetectorRef, inject, OnInit } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router'; // <-- AÑADIR RouterModule
 import { ToastsComponent } from "./components/toasts/toasts";
 import { CommonModule } from '@angular/common';
@@ -13,11 +13,21 @@ import { SupabaseService } from './services/supabase';
 })
 
 
-export class App {
-  protected readonly title = signal('cotizador-OF');
-  isMenuCollapsed = true; // <-- AÑADE ESTA LÍNEA
-   private supabaseService = inject(SupabaseService);
+export class App implements OnInit {
+  isMenuCollapsed = true;
+  isLoggedIn = false; // <-- Nueva propiedad para controlar la visibilidad
+
+  private supabaseService = inject(SupabaseService);
   private router = inject(Router);
+  private cdr = inject(ChangeDetectorRef);
+
+  ngOnInit(): void {
+    // Escuchamos los cambios en la sesión de autenticación
+    this.supabaseService.supabase.auth.onAuthStateChange((event, session) => {
+      this.isLoggedIn = !!session; // Si hay sesión, isLoggedIn es true. Si no, es false.
+      this.cdr.detectChanges(); // Forzamos la detección de cambios
+    });
+  }
 
   async signOut(): Promise<void> {
     await this.supabaseService.signOut();
