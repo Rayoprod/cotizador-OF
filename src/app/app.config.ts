a1c9b46f5cf5f8c28403206581efd4bf50a0d4f4
Agrovictoria <rrodriguez.1507w@gmail.com>
2025-08-11 17:59:32 -0500

    a
    a

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,24 @@
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { importProvidersFrom } from '@angular/core';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(NgbModule), // <-- AÑADE ESTA LÍNEA AL PRINCIPIO
+
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
+
+    provideRouter(routes), provideHttpClient(withFetch()), // <-- AÑADE ESTA LÍNEA
+
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
